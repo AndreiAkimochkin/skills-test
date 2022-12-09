9a4bea0357ea26ab9bc672b4c78268e5528a642b
Andrei <AndreiAkimochkin1985@gmail.com>
2022-12-09 20:32:51 +0300

    fix router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { HashRouter, Route, Routes} from "react-router-dom";
 import Step1 from "./components/Step1";
 import Step2 from "./components/Step2";
 import Step3 from "./components/Step3";
@@ -10,14 +10,14 @@ function App() {
     return (
         <>
             <Header/>
-            <BrowserRouter>
+            <HashRouter>
                 <Routes>
                     <Route path="/" element={<Step1/>}/>
                     <Route path="step2" element={<Step2/>}/>
                     <Route path="step3" element={<Step3/>}/>
                     <Route path="result" element={<Result/>}/>
                 </Routes>
-            </BrowserRouter>
+            </HashRouter>
             <Footer/>
         </>
     );
